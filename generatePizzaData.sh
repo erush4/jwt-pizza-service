b326786d8b70efa1e8e19fdc5d4ac555d3e0a18a
erush4 <=>
2026-02-03 22:40:06 -0700

    extract data from shell script

--- generatePizzaData.sh
+++ generatePizzaData.sh
@@ -6,12 +6,22 @@ if [ -z "$1" ]; then
 fi
 host=$1
 
-response=$(curl -s -X PUT $host/api/auth -d '{"email":"a@jwt.com", "password":"admin"}' -H 'Content-Type: application/json')
-token=$(echo $response | jq -r '.token')
+CONFIG=$(node print-config.js)
+
+ADMIN_EMAIL=$(echo $CONFIG | jq -r '.adminEmail')
+ADMIN_PASSWORD=$(echo $CONFIG | jq -r '.adminPassword')
+
+DINER_EMAIL=$(echo $CONFIG | jq -r '.dinerEmail')
+DINER_PASSWORD=$(echo $CONFIG | jq -r '.dinerPassword')
+
+FRANCHISE_EMAIL=$(echo $CONFIG | jq -r '.franchiseEmail')
+FRANCHISE_PASSWORD=$(echo $CONFIG | jq -r '.franchisePassword')
+
+response=$(curl -s -X PUT $host/api/auth -H 'Content-Type: application/json' -d '{"email":"$ADMIN_EMAIL", "password":"$ADMIN_PASSWORD"}')token=$(echo $response | jq -r '.token')
 
 # Add users
-curl -X POST $host/api/auth -d '{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json'
-curl -X POST $host/api/auth -d '{"name":"pizza franchisee", "email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth  -d '{"name":"pizza diner", "email":"$DINER_EMAIL", "password":"$DINER_PASSWORD"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"pizza franchisee", "email":"$FRANCHISE_EMAIL", "password":"$FRANCHISE_PASSWORD"}' -H 'Content-Type: application/json'
 
 # Add menu
 curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Veggie", "description": "A garden of delight", "image":"pizza1.png", "price": 0.0038 }'  -H "Authorization: Bearer $token"
@@ -21,7 +31,7 @@ curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "titl
 curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Charred Leopard", "description": "For those with a darker side", "image":"pizza5.png", "price": 0.0099 }'  -H "Authorization: Bearer $token"
 
 # Add franchise and store
-curl -X POST $host/api/franchise -H 'Content-Type: application/json' -d '{"name": "pizzaPocket", "admins": [{"email": "f@jwt.com"}]}'  -H "Authorization: Bearer $token"
+curl -X POST $host/api/franchise -H 'Content-Type: application/json' -d '{"name": "pizzaPocket", "admins": [{"email": "$FRANCHISE_EMAIL"}]}'  -H "Authorization: Bearer $token"
 curl -X POST $host/api/franchise/1/store -H 'Content-Type: application/json' -d '{"franchiseId": 1, "name":"SLC"}'  -H "Authorization: Bearer $token"
 
 echo "Database data generated"
